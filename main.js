42c31607b837104ead9d905589d07176b4cc2657
Marina Gotovtseva <ceruleanblue44@gmail.com>
2022-01-10 18:43:36 +0300

    Before going home changes

--- main.js
+++ main.js
@@ -12,12 +12,22 @@ class Book {
 }
 
 const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet');
+const learningJS = new Book('Learning JavaScript', 'Ethan Brown', 340, 'not read yet');
 
-console.log(theHobbit.info());
+// console.log(theHobbit.info());
 
-let myLibrary = [theHobbit];
+let myLibrary = [];
 
-function addBookToLibrary() {
+const addBookToLibrary = (event) => {
+  let bookTitle = document.getElementById('book-title').value;
+  let bookAuthor = document.getElementById('book-author').value;
+  let bookPages = document.getElementById('book-pages').value;
+  console.log(bookTitle, bookAuthor, bookPages);
+  // return bookTitle, bookAuthor, bookPages;
+  event.preventDefault();
+  let book = new Book(bookTitle, bookAuthor, bookPages);
+  myLibrary.push(book);
+  console.log(book);
 
 }
 
@@ -26,6 +36,22 @@ let bookCard = document.getElementById('book-card');
 bookCard.innerHTML = myLibrary.map(book => {
   return `<h3 class="book__title js-title">${book.title}</h3 > 
           <p class="book__author js-author">${book.author}</p>
-          <p class="book__pages js-pages">Pages: ${book.pages}</p>
-          <button type="submit">Remove</button>`
-})
+          <p class="book__pages js-pages">Pages: ${book.pages}</p>`
+});
+
+// Show modal with forms
+document.getElementById('new-book').addEventListener('click', () => {
+  document.getElementById('modal').classList.remove('d-none');
+});
+
+// Collect user input from forms
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('get-input').addEventListener('click', addBookToLibrary);
+});
+
+
+// window.addEventListener('DOMContentLoaded', (event) => {
+//   console.log('DOM fully loaded and parsed');
+// });
+
+// https://www.youtube.com/watch?v=NxVCq4p0Kb0
